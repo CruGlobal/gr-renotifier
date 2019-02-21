8d478183f5d5068fdeac7fde58080fa399f2d84a
Matt Drees <matt.drees@cru.org>
2019-02-21 13:50:13 -0700

    Start with a simple maven project
    This is generated from:
    mvn archetype:generate \
      -DarchetypeArtifactId=maven-archetype-quickstart \
      -Dproject-app-name=gr-renotifier \
      -DgroupId=org.cru.globalreg.renotifier \
      -DartifactId=gr-renotifier \
      -Dpackage=org.cru.globalreg.renotifier \
      -DinteractiveMode=false \
      -Dversion=0.1.0-SNAPSHOT

---
+++ src/main/java/org/cru/globalreg/renotifier/App.java
@@ -0,0 +1,13 @@
+package org.cru.globalreg.renotifier;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
